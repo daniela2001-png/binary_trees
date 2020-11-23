2c524d430b7dd7167df5939bd78d14e5806c79e2
afarizap <afarizap@gmail.com>
2020-11-23 20:17:46 +0000

    add task 0 new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,14 @@
  * @value:  value to put in the new node
  * Return: a pointer to the new node (binary_tree_t), or NULL on failure
  */
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-     Return 0 ;
- }
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	return (new_node);
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # binary_trees
 0x1D. C - Binary trees
+
+- Compile with ´gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 0-main.c 0-binary_tree_node.c -o 0-node´
