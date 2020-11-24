b2ca9246e573e7b1ca2aadb8db9717c0002d25d4
daniela2001-png <dmbmora@outlook.com>
2020-11-24 15:27:59 -0500

    [new] adding changes to point 9 without leaves conditional

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to measure the height.
@@ -7,13 +15,14 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t level = 0;
+	//size_t level = 0;
+	size_t der, izq = 0;
 
-	if (!tree)
+	if (!tree || binary_tree_is_leaf(tree))
 		return (0);
-	if (tree->left)
-		binary_tree_height(tree->left), level++;
-	if (tree->right)
-		binary_tree_height(tree->right), level++;
-	return (level);
+	der = binary_tree_height(tree->right);
+	izq = binary_tree_height(tree->left);
+	if (der > izq)
+		return der + 1;
+	return izq + 1;
 }
