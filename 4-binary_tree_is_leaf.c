c0c686af1d10c8758a475ffeada4d71588a3731e
afarizap <afarizap@gmail.com>
2020-11-26 17:05:55 +0000

    check if node exist 2

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,9 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->left && !node->right && node)
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
 		return (1);
 	return (0);
 }
