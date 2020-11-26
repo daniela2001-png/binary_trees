d7708210aacb1b3bbc93b12d6cba406fbe305c6c
afarizap <afarizap@gmail.com>
2020-11-26 16:45:33 +0000

    fix sizeof and sintax

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,9 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *my_node = NULL;
+	binary_tree_t *my_node = malloc(sizeof(binary_tree_t));
 
-	if (!parent)
-		return (NULL);
-	my_node = malloc(sizeof(struct binary_tree_s));
-	if (!my_node)
+	if (!parent || !my_node)
 		return (NULL);
 	my_node->left = NULL;
 	my_node->right = NULL;
