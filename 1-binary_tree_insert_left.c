518fa116d3345ced3a5ffda9bd8c823c475e0326
afarizap <afarizap@gmail.com>
2020-11-26 15:44:51 +0000

    fix ´if parent is null´

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *my_node = malloc(sizeof(struct binary_tree_s));
 
-	if (my_node == NULL)
+	if (!my_node || !parent)
 		return (NULL);
 	my_node->n = value;
 	my_node->left = NULL;
