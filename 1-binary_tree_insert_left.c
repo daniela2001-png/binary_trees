d59490a7c6c887156a6f4ca479ff338b8ab88bc0
daniela2001-png <dmbmora@outlook.com>
2020-11-26 11:22:57 -0500

    [new] change the value initial to pointer my_node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,35 +2,29 @@
 
 /**
  * binary_tree_insert_left - insert a node to left
- * @parent: is a pointer to my new_node
+ * @parent: is a pointer to my my_node
  * @value: is the value to insert inside the node
- * Return: the pointer to a new_node to left
+ * Return: the pointer to a my_node to left
 */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *my_node = NULL;
 
-	my_node = malloc(sizeof(struct binary_tree_s));
-
+	if (!parent)
+		return (NULL);
+	my_node = malloc(sizeof(binary_tree_t));
 	if (!my_node)
 		return (NULL);
-	if (parent == NULL)
-		return (NULL);
-
 	my_node->left = NULL;
 	my_node->right = NULL;
 	my_node->parent = parent;
 	my_node->n = value;
-
 	if (parent->left)
 	{
-		/* left del 54 es 12*/
 		my_node->left = parent->left;
-		/* aqui el padre de 12 pase de ser de 98 a 54 :3 */
 		my_node->left->parent = my_node;
 	}
-	/* aqui asigno la izquierda al padre si o si siempre */
 	parent->left = my_node;
 	return (my_node);
 }
